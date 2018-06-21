5940eda1588d6ab0c77c75f01974617ac96dbd30
Zhang Peng <forbreak@163.com>
2018-06-21 15:29:59 +0800

    :bookmark: demo01 - 身份认证

---
+++ shiro-demo01/src/main/java/io/github/dunwu/shiro/demo01/ShiroDemosApplication.java
@@ -0,0 +1,12 @@
+package io.github.dunwu.shiro.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemosApplication.class, args);
+    }
+}
